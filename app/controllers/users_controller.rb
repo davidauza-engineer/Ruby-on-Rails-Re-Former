f74765c4a6fdd8639e10efb32554e8d2702588bb
Luis Novoa <luis.matte.novoa@gmail.com>
2020-02-03 18:33:16 -0400

    Codes edit to show errors if the validations are not met

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User succesfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
